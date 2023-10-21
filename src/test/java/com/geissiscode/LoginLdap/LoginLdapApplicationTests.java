b653b7a77d0aa9ca1e13307cb6ea7421b4a1479a
geissi <hermi.holunder@gmail.com>
2023-10-21 14:30:07 +0200

    initial commit

---
+++ src/test/java/com/geissiscode/LoginLdap/LoginLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geissiscode.LoginLdap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
